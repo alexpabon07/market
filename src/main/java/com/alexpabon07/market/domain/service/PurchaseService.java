c6fb122cb94261d9203183d9d6032ae28e4f7207
Alexander Pabon <alex312-@hotmail.com>
2022-04-12 20:16:08 -0500

    Se implementan el service de PurchaseRepository

---
+++ src/main/java/com/alexpabon07/market/domain/service/PurchaseService.java
@@ -0,0 +1,28 @@
+package com.alexpabon07.market.domain.service;
+
+import com.alexpabon07.market.domain.Purchase;
+import com.alexpabon07.market.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    public List<Purchase> getAll() {
+        return purchaseRepository.getAll();
+    }
+
+    public Optional<List<Purchase>> getByClient(String idClient) {
+        return purchaseRepository.getByClient(idClient);
+    }
+
+    public Purchase save(Purchase purchase) {
+        return purchaseRepository.save(purchase);
+    }
+}
